b85f1380d08062a97642168c049c3a8e1c042f45
manshikapatel <patelmanshikac62@gmail.com>
2024-07-26 12:40:49 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,5 @@
 def decode_message( s: str, p: str) -> bool:
 
 # write your code here
-        if len(input_string) != len(pattern):
+        if len(s) != len(pattern):
                 return False
