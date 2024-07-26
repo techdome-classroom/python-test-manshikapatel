d66263a1f65d218dbcfc806d71b66ab40bb94c71
manshikapatel <patelmanshikac62@gmail.com>
2024-07-26 13:02:39 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,7 +4,7 @@ def decode_message(s: str, p: str) -> bool:
             return si == len(s)
         
         if si == len(s):
-            # If s is finished but p is not, check if remaining p is all '*'
+            
             return all(p[k] == '*' for k in range(pi, len(p)))
         
         if p[pi] == s[si] or p[pi] == '?':
